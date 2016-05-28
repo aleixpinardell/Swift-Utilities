36cc232a84f2e07357db17874e370f6f83fb928e
Aleix Pinardell <aleixpinardell@gmail.com>
2016-05-28 11:35:40 +0200

    Create centerVertically.swift

---
+++ Extensions/NSTextAttachment/centerVertically.swift
@@ -0,0 +1,16 @@
+extension NSTextAttachment {
+    /**
+     Centers `self.image` vertically based on a provided `font`.
+   
+     - parameters:
+        - font: The font used to determine the vertical origin of the `self.image`.
+   
+     - note: If `self.image` if not set, this method does nothing.
+    */
+    func centerVertically(font font: UIFont) {
+        if let image = self.image {
+            let mid = font.descender + font.capHeight
+            self.bounds.origin.y = round(font.descender - image.size.height/2 + mid + 3)
+        }
+    }
+}
